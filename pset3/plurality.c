e86c4557ef530356a4a262d1fd8aa9fcebee11ac
Inocybe <levmitchell@icloud.com>
2023-09-15 13:55:46 -0700

    it works!

--- pset3/plurality.c
+++ pset3/plurality.c
@@ -98,8 +98,8 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (high = candidates[i].votes)
-            printf("%s\n", candidates[i].name)
+        if (high == candidates[i].votes)
+            printf("%s\n", candidates[i].name);
     }
     
     return;
