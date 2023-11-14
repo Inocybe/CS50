b27e2bf309e05e8aa0ae2fdd3cb808f59ee36db8
Inocybe <levmitchell@icloud.com>
2023-11-14 13:30:46 -0800

    greyscale works

--- pset4/filters/helpers.c
+++ pset4/filters/helpers.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //find the highest value rbg then make all the other colors match
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+            int average = (red + green + blue) / 3;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
     return;
 }
 
