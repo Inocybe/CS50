02e60c8cde4996b366a0ccc53007191aa6b65836
Inocybe <levmitchell@icloud.com>
2023-11-14 11:05:17 -0800

    a

--- pset4/volume/volume.c
+++ pset4/volume/volume.c
@@ -39,11 +39,11 @@ int main(int argc, char *argv[])
     fwrite(header, HEADER_SIZE, 1, output);
     
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t buffer;
-    while (fread(&buffer, sizeof(uint16_t), 1, input))
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
-        fwrite(&buffer, sizeof(uint16_t), 1, output);
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
     // Close files
