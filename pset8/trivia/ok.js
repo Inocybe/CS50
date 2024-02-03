dfc37d70f0039c8f16204588a5df8137c4728cbe
Inocybe <140019717+Inocybe@users.noreply.github.com>
2024-02-02 20:10:35 -0800

    yuyu

--- pset8/trivia/ok.js
+++ pset8/trivia/ok.js
@@ -1,7 +1,11 @@
 const wrongButtons = document.querySelectorAll("#wrong-button");
 wrongButtons.forEach(button => {
     button.addEventListener("click", function() {
+        let originalColor = this.style.backgroundColor;
         this.style.backgroundColor = "red";
+        setTimeout(() => {
+            this.style.backgroundColor = originalColor;
+        }, 2000);
     });
 });
 
